448e8da9edea2eec275c406015ab1df8c673d91c
Benedek Farkas <bfarkas@windowslive.com>
2014-06-27 20:02:56 +0200

    Adding missing stylesheet and fixing ResourceManifest

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -6,14 +6,14 @@ namespace PJS.FlatJack {
             var manifest = builder.Add();
 
             manifest.DefineScript("ThemeScript").SetUrl("script.min.js").SetDependencies("jQuery");
-            manifest.DefineScript("ValidationEngine-en").SetUrl("jquery.validationEngine-en.js").SetVersion("2.6.2").SetDependencies("jQuery");
-            manifest.DefineScript("ValidationEngine").SetUrl("jquery.validationEngine.js").SetVersion("2.6.2").SetDependencies("ValidationEngine-en");
+            manifest.DefineScript("Theme-ValidationEngine-en").SetUrl("jquery.validationEngine-en.js").SetVersion("2.6.2").SetDependencies("jQuery");
+            manifest.DefineScript("Theme-ValidationEngine").SetUrl("jquery.validationEngine.js").SetVersion("2.6.2").SetDependencies("Theme-ValidationEngine-en");
             manifest.DefineScript("Isotope").SetUrl("jquery.isotope.min.js").SetVersion("1.5.25").SetDependencies("jQuery");
             manifest.DefineScript("prettyPhoto").SetUrl("jquery.prettyPhoto.js").SetVersion("3.1.5").SetDependencies("jQuery");
             manifest.DefineScript("Styler").SetUrl("jquery.styler.js").SetDependencies("jQuery");
 
             manifest.DefineStyle("ThemeStyle").SetUrl("style.min.css");
-            manifest.DefineStyle("ValidationEngine").SetUrl("validationEngine.jquery.css");
+            manifest.DefineStyle("Theme-ValidationEngine").SetUrl("validationEngine.jquery.css");
             manifest.DefineStyle("prettyPhoto").SetUrl("prettyPhoto.css");
             manifest.DefineStyle("Bootstrap-Social").SetUrl("bootstrap-social.css");
         }
